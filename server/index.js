40b77bdef16ee9aa49a6255759eba8d7fdc9dfc9
Travroz <gustavo.espinozap@alumnos.uv.cl>
2026-04-29 13:06:17 -0400

    Ajuste de CORS y URL de producción

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const nodemailer = require('nodemailer'); // Importamos nodemailer
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 // Configuración del "Transportador" de correos
